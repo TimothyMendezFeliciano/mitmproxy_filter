485d7006b942cb047ab35f16180a2556e469efcb
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2025-03-05 10:30:03 -0400

    Main

--- mitmproxy_filter.py
+++ mitmproxy_filter.py
@@ -2,6 +2,12 @@ import asyncio
 from mitmproxy import http, options
 from mitmproxy.tools.dump import DumpMaster
 
+# import logging
+
+# Configure logging
+# logging.basicConfig(filename="blocked_requests.log", level=logging.INFO, format="%(asctime)s - %(message)s")
+
+
 # List of adult domains to block
 BLOCKED_DOMAINS = [
     "pornhub.com", "xvideos.com", "redtube.com", "xnxx.com", "xxx.com",
@@ -10,16 +16,37 @@ BLOCKED_DOMAINS = [
     "serviporno.com", "xvideos2.com", "bellesa.co", "xxxi.porn"
 ]
 
+# List of WhatsApp domains to allow
+WHITELISTED_DOMAINS = [
+    "mmg.whatsapp.net",
+    "media.whatsapp.net",
+    "static.whatsapp.net",
+    "web.whatsapp.com",
+    "www.google.com",
+    "ssl.gstatic.com",
+    "beacons.gcp.gvt2.com",
+    "mtalk.google.com",
+    "clientservices.googleapis.com",
+]
+
+
 class BlockAdultSites:
     def request(self, flow: http.HTTPFlow) -> None:
         # Intercept requests and block adult sites
+
+        for allowed_domain in WHITELISTED_DOMAINS:
+            if allowed_domain in flow.request.pretty_url:
+                return
+
         for domain in BLOCKED_DOMAINS:
             if domain in flow.request.pretty_url:
+                # logging.info(f"Blocked: {flow.request.pretty_url}")
                 flow.response = http.Response.make(
-                    403, # Http Status Code - Forbidden
+                    403,  # Http Status Code - Forbidden
                     b"Blocked by mitmproxy",
                     {"Content-Type": "text/plain"}
                 )
+
 
 class MitmProxyRunner:
     def __init__(self):
@@ -45,6 +72,7 @@ class MitmProxyRunner:
             self.loop.run_until_complete(self.master.shutdown())
         finally:
             self.loop.close()
+
 
 if __name__ == "__main__":
     proxy = MitmProxyRunner()
